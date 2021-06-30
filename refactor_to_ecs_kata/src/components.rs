5e4f5fc03806cd5d421bf6b13cf269a8c6148dae
sousajo <sousajo@pop-os.localdomain>
2021-06-30 13:29:23 +0100

    facing as component

--- refactor_to_ecs_kata/src/components.rs
+++ refactor_to_ecs_kata/src/components.rs
@@ -5,11 +5,12 @@ use crate::sprite::Sprite;
 
 
 pub struct PlayerCharacter {
-    position: Vec3,
-    speed: f32,
-    facing: Direction,
-    sprite: Sprite,
+    pub position: Vec3,
+    pub speed: f32,
+    pub sprite: Sprite,
 }
+
+pub struct Facing(pub Direction);
 
 impl PlayerCharacter {
 
@@ -19,29 +20,12 @@ impl PlayerCharacter {
         PlayerCharacter {
             position: Vec3::new(0.0, 0.0, 1.0),
             speed: 0.0,
-            facing: Direction::Right,
             sprite: PLAYER_SPRITE,
         }
     }
 
-    pub fn update_sprite_index(&mut self) -> u32 {
-        if self.speed == 0.0 {
-            self.sprite.current_index = self.still_sprite_index();
-        } else {
-            self.sprite.current_index += 1;
-            if self.sprite.current_index % self.sprite.n_columns as u32 == 0 {
-                self.sprite.current_index -= self.sprite.n_columns as u32;
-            }
-        };
-        self.sprite.current_index
-    }
 
-    pub fn set_direction(&mut self, direction: Direction) {
-        if self.facing != direction {
-            self.facing = direction;
-            self.sprite.current_index = self.still_sprite_index();
-        };
-    }
+
 
     pub fn decrease_speed(&mut self, dt: f32) {
         self.speed -= 240.0 * dt;
@@ -57,19 +41,6 @@ impl PlayerCharacter {
         };
     }
 
-    pub fn update_position(&mut self, dt: f32) -> Vec3 {
-        let ds = dt * self.speed * self.facing.as_vec();
-        self.position += ds;
-        self.position
-    }
 
-    fn still_sprite_index(&self) -> u32 {
-        match &self.facing {
-            Direction::Down => 0,
-            Direction::Up => 4,
-            Direction::Left => 8,
-            Direction::Right => 13, //wtf
-        }
-    }
 
 }
--- refactor_to_ecs_kata/src/systems.rs
+++ refactor_to_ecs_kata/src/systems.rs
@@ -1,6 +1,6 @@
 use bevy::prelude::*;
 
-use crate::components::PlayerCharacter;
+use crate::components::{PlayerCharacter, Facing};
 use crate::direction::Direction;
 
 
@@ -23,26 +23,54 @@ pub fn load_player_sprite_sheet(
             ..Default::default()
         })
         .insert(Timer::from_seconds(PLAYER_SPRITE.frame_time, true))
-        .insert(PlayerCharacter::new());
+        .insert(PlayerCharacter::new())
+        .insert(Facing(Direction::Right));
+}
+pub fn still_sprite_index(facing: &mut Facing) -> u32 {
+    match &facing.0 {
+        Direction::Down => 0,
+        Direction::Up => 4,
+        Direction::Left => 8,
+        Direction::Right => 13, //wtf
+    }
+}
+
+pub fn set_direction(player: &mut PlayerCharacter, facing: &mut Facing, direction: Direction) {
+    if facing.0 != direction {
+        facing.0 = direction;
+        player.sprite.current_index = still_sprite_index(facing);
+    };
+}
+
+pub fn update_sprite_index(player: &mut PlayerCharacter, facing: &mut Facing) -> u32 {
+    if player.speed == 0.0 {
+        player.sprite.current_index = still_sprite_index(facing);
+    } else {
+        player.sprite.current_index += 1;
+        if player.sprite.current_index % player.sprite.n_columns as u32 == 0 {
+            player.sprite.current_index -= player.sprite.n_columns as u32;
+        }
+    };
+    player.sprite.current_index
 }
 
 pub fn input(
     time: Res<Time>,
     input: Res<Input<KeyCode>>,
-    mut query: Query<&mut PlayerCharacter>,
+    mut query: Query<(&mut PlayerCharacter, &mut Facing)>,
 ) {
-    for mut char in query.iter_mut() {
+    for (mut char, mut facing) in query.iter_mut() {
         if input.pressed(KeyCode::Left) {
-            char.set_direction(Direction::Left);
+            set_direction(&mut char, &mut facing, Direction::Left);
             char.increase_speed(time.delta_seconds());
         } else if input.pressed(KeyCode::Up) {
-            char.set_direction(Direction::Up);
+            set_direction(&mut char, &mut facing, Direction::Up);
             char.increase_speed(time.delta_seconds());
         } else if input.pressed(KeyCode::Down) {
-            char.set_direction(Direction::Down);
+            set_direction(&mut char, &mut facing, Direction::Down);
             char.increase_speed(time.delta_seconds());
         } else if input.pressed(KeyCode::Right) {
-            char.set_direction(Direction::Right);
+            set_direction(&mut char, &mut facing, Direction::Right);
             char.increase_speed(time.delta_seconds());
         } else {
             char.decrease_speed(time.delta_seconds());
@@ -50,26 +78,33 @@ pub fn input(
     }
 }
 
-pub fn movement(
+pub fn update_position(player: &mut PlayerCharacter, facing: &mut Facing, dt: f32) -> Vec3 {
+    let ds = dt * player.speed * facing.0.as_vec();
+    player.position += ds;
+    player.position
+}
+
+    pub fn movement(
     time: Res<Time>,
-    mut query: Query<(&mut PlayerCharacter, &mut Transform)>,
+    mut query: Query<(&mut PlayerCharacter, &mut Transform, &mut Facing)>,
 ) {
-    for (mut char, mut transform) in query.iter_mut() {
-        let position = char.update_position(time.delta_seconds());
+    for (mut char, mut transform, mut facing) in query.iter_mut() {
+        let position = update_position(&mut char, &mut facing, time.delta_seconds());
         let translation = &mut transform.translation;
         translation.x = position.x;
         translation.y = position.y;
     }
 }
 
+
 pub fn sprite(
     time: Res<Time>,
-    mut query: Query<(&mut PlayerCharacter, &mut Timer, &mut TextureAtlasSprite)>,
+    mut query: Query<(&mut PlayerCharacter, &mut Timer, &mut TextureAtlasSprite, &mut Facing)>,
 ) {
-    for (mut char, mut timer, mut sprite) in query.iter_mut() {
+    for (mut char, mut timer, mut sprite, mut facing) in query.iter_mut() {
         timer.tick(time.delta());
         if timer.finished() {
-            sprite.index = char.update_sprite_index();
+            sprite.index = update_sprite_index(&mut char, &mut facing);
         }
     }
 }
